6e53f29987b384d2c5a3c3634ec836bfd68d96dc
Tamer Wagih <mrtamerwagih@gmail.com>
2021-02-19 01:25:17 +0300

    Adding home, orders, check out views, controllers and routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
 
-router.post('/add-product', productsController.postAddProduct);
+router.get('/products', adminController.getProducts);
+
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
